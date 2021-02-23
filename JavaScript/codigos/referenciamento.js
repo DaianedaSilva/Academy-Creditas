3cc1d722fd836195626d8d1e81d06c3a591a0dbb
Daiane <daianedasilva044@gmail.com>
2021-02-23 13:31:29 -0300

    atualização

---
+++ JavaScript/codigos/referenciamento.js
@@ -0,0 +1,26 @@
+/*let numero = 6;
+
+
+function doMagic(number){
+    number = 8;
+    return number
+}
+
+
+console.log(numero)
+numero = doMagic(numero)
+console.log(numero)*/
+
+
+let pessoa = {
+    nome: "Diandria"
+}
+
+function Magic(people){
+    people.nome = "daiane"
+}
+
+console.log(pessoa)
+Magic(pessoa)
+console.log(pessoa)
+
