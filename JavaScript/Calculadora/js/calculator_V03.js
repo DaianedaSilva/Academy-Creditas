0d681f97255174aa8e9dd42d2f926d9caaa21b8b
Daiane <daianedasilva044@gmail.com>
2021-03-10 11:28:07 -0300

    Calculadora final

---
+++ JavaScript/Calculadora/js/calculator_V03.js
@@ -0,0 +1,132 @@
+
+let number1;
+let number2;
+let operator;
+const screen = document.getElementById('screen');
+let lastValue = " ";
+
+const CaptureValue = (value)=>{
+
+    if (checkNumber(value) || value=="."){
+
+        if (lastValue == 'operator'){
+            ClearScreen()
+        }
+    
+        ShowOnScreen(value)
+        LastValue("number")
+    }else {
+
+        CaptureOperator(value)
+        
+    }
+}
+
+
+const CaptureOperator = (value_operator) =>{
+    
+    if(value_operator == "CE"){
+        ClearScreen()
+
+    }else if(value_operator == "="){
+        if (lastValue == "operator"){
+            capturerNumbe1()
+            calculate(operator)
+
+        }else{
+            capturerNumbe2()
+            calculate(operator)
+        } 
+
+    }else{
+        operator = value_operator
+        capturerNumbe1() 
+    }
+ 
+    console.log("ultimo apertado:",lastValue)
+
+   console.log(operator)
+   LastValue("operator")
+
+}
+
+const calculate = (operator)=>{
+
+    console.log("calculete:", operator)
+
+    let result;
+    number1 = parseFloat(number1)
+    number2 = parseFloat(number2)
+    console.log("number1:", number1)
+    console.log("number2:", number2)
+
+    switch (operator){
+        case '+':
+            result =  number1 + number2
+            console.log("result:", result)
+            ClearScreen()
+            ShowOnScreen(result)
+            return
+        case '-': 
+            result =  number1 - number2
+            ClearScreen()
+            ShowOnScreen(result)
+            return
+        case '/': 
+            if(number2 == 0){
+                ClearScreen()
+                ShowOnScreen("ERROR DIVISÃO 0")
+                setTimeout(ClearScreen,600)
+                
+            }else{
+                result = number1/number2 
+                ClearScreen()
+                ShowOnScreen(result)
+            }
+            return
+        case 'x':
+            result = (number1*number2)
+            ClearScreen()
+            ShowOnScreen(result)
+            return
+    }
+}
+
+const capturerNumbe1 = () =>{
+
+    
+    let number = screen.innerHTML;
+
+    number1 = number;
+    console.log('Capturando number1:', number1)
+        
+}
+
+const capturerNumbe2 = () =>{
+    let number = screen.innerHTML;
+    number2 = number;
+    console.log(" Capturando number2:", number2)
+}
+
+const LastValue = (value) =>{
+    lastValue = value;
+}
+
+const ClearScreen = ()=>{
+    
+    screen.innerHTML = " ";
+}
+
+const ShowOnScreen = (number) =>{
+    
+    screen.innerHTML += number;
+}
+
+const checkNumber = (value)=>{
+    if (isNaN(value)){
+        return false
+    }else{
+        return true
+    }
+}
+
